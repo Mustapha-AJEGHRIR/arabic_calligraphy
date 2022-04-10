c12836878bf23666393278f5409468b2e0cbbc46
Nouamane Tazi <nouamane98@gmail.com>
2022-04-10 02:42:08 +0200

    quick fix

--- scripts/TrOCR.py
+++ scripts/TrOCR.py
@@ -1,19 +1,20 @@
-
 # %% [markdown]
 # ## Prepare data
 
 # %%
 import wandb
+
 wandb.init(project="TrOCR")
 
 # %%
 import pandas as pd
 import glob
 import os
+
 data_path = "../data/calliar/chars/"
-images = glob.glob(os.path.join(data_path, '*'))[:3000]
+images = glob.glob(os.path.join(data_path, "*"))[:32]
 df = pd.DataFrame(images, columns=["file_name"])
-df['text'] = df['file_name'].apply(lambda x: x.split("/")[-1].split(":")[-1][:-4]) # ":" or "\uf03a"
+df["text"] = df["file_name"].apply(lambda x: x.split("/")[-1].split(":")[-1][:-4])  # ":" or "\uf03a"
 df
 
 # %%
@@ -31,6 +32,7 @@ import torch
 from torch.utils.data import Dataset
 from PIL import Image
 
+
 class IAMDataset(Dataset):
     def __init__(self, root_dir, df, processor, max_target_length=128):
         self.root_dir = root_dir
@@ -42,32 +44,27 @@ class IAMDataset(Dataset):
         return len(self.df)
 
     def __getitem__(self, idx):
-        # get file name + text 
-        file_name = self.df['file_name'][idx]
-        text = self.df['text'][idx]
+        # get file name + text
+        file_name = self.df["file_name"][idx]
+        text = self.df["text"][idx]
         # prepare image (i.e. resize + normalize)
         image = Image.open(file_name).convert("RGB")
         pixel_values = self.processor(image, return_tensors="pt").pixel_values
         # add labels (input_ids) by encoding the text
-        labels = self.processor.tokenizer(text, 
-                                          padding="max_length", 
-                                          max_length=self.max_target_length).input_ids
+        labels = self.processor.tokenizer(text, padding="max_length", max_length=self.max_target_length).input_ids
         # important: make sure that PAD tokens are ignored by the loss function
         labels = [label if label != self.processor.tokenizer.pad_token_id else -100 for label in labels]
 
         encoding = {"pixel_values": pixel_values.squeeze(), "labels": torch.tensor(labels)}
         return encoding
 
+
 # %%
 from transformers import TrOCRProcessor
 
 processor = TrOCRProcessor.from_pretrained("microsoft/trocr-small-stage1")
-train_dataset = IAMDataset(root_dir=data_path,
-                           df=train_df,
-                           processor=processor)
-eval_dataset = IAMDataset(root_dir=data_path,
-                           df=test_df,
-                           processor=processor)
+train_dataset = IAMDataset(root_dir=data_path, df=train_df, processor=processor)
+eval_dataset = IAMDataset(root_dir=data_path, df=test_df, processor=processor)
 
 # %%
 print("Number of training examples:", len(train_dataset))
@@ -75,15 +72,15 @@ print("Number of validation examples:", len(eval_dataset))
 
 # %%
 encoding = train_dataset[0]
-for k,v in encoding.items():
-  print(k, v.shape)
+for k, v in encoding.items():
+    print(k, v.shape)
 
 # %%
-image = Image.open(train_df['file_name'][0]).convert("RGB")
+image = Image.open(train_df["file_name"][0]).convert("RGB")
 image
 
 # %%
-labels = encoding['labels']
+labels = encoding["labels"]
 labels[labels == -100] = processor.tokenizer.pad_token_id
 label_str = processor.decode(labels, skip_special_tokens=True)
 print("label_str", label_str)
@@ -109,7 +106,7 @@ model.config.max_length = 64
 model.config.early_stopping = True
 model.config.no_repeat_ngram_size = 3
 model.config.length_penalty = 2.0
-model.config.num_beams = 1 # 4
+model.config.num_beams = 1  # 4
 
 # %%
 from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
@@ -119,13 +116,13 @@ training_args = Seq2SeqTrainingArguments(
     evaluation_strategy="steps",
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
-    fp16=True, 
+    fp16=True,
     output_dir="./",
-    logging_steps=2,
-    save_steps=1000,
+    logging_steps=1,
+    save_steps=1,
     save_total_limit=1,
-    eval_steps=10,
-    num_train_epochs=1000,
+    eval_steps=1,
+    num_train_epochs=1,
     report_to="wandb",
 )
 
@@ -146,6 +143,7 @@ def compute_metrics(pred):
 
     return {"cer": cer}
 
+
 # %%
 from transformers import default_data_collator
 
@@ -162,6 +160,3 @@ trainer = Seq2SeqTrainer(
 trainer.train()
 
 # %%
-
-
-
