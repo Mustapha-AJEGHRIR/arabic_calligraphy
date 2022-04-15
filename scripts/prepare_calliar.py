73703e4fae8fc655226c83a70e4361ba5abf638e
Nouamane Tazi <nouamane98@gmail.com>
2022-04-16 01:59:17 +0200

    refactor prepare_calliar

--- scripts/prepare_calliar.py
+++ scripts/prepare_calliar.py
@@ -1,29 +1,17 @@
-# %%
-import numpy as np
-import pickle
-import io
-import glob
-import json
 import base64
-from utils.vis import *
-import numpy as np
-import matplotlib.pyplot as plt
-from IPython.core.display import display, HTML, Video
+import glob
+import io
+import json
 import os
+import pickle
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.core.display import HTML, Video, display
+import argparse
 
-# %%
-dataset_path = "../Calliar/"
-npy_files = glob.glob(os.path.join(dataset_path, "dataset/train/**.json"))
+from utils.vis import *
 
-# Preview a single file
-json_path = np.random.choice(npy_files)
-drawing = json.load(open(json_path))
-print(get_annotation(json_path))
-data, _ = convert_3d(drawing, return_flag=True, threshold=50)
-draw_strokes(data, stroke_width=8, crop=True, square=True)
-
-
-# %%
 # ## Character-level strokes
 def draw_chars(json_path, plot=False, save_folder="../data/calliar/chars/", stroke_width=3, crop=True, square=True):
     if not os.path.exists(save_folder):
@@ -43,40 +31,28 @@ def draw_chars(json_path, plot=False, save_folder="../data/calliar/chars/", stro
             display(im)
 
 
-draw_chars(json_path, plot=True)
-
-# %%
-from tqdm import tqdm
-
-for json_path in tqdm(npy_files):
-    draw_chars(json_path, plot=False, stroke_width=np.random.randint(2, 8))
-
-# %%
-# plot a random sample of characters
-import glob
-import matplotlib.pyplot as plt
-
-plt.rcParams["figure.figsize"] = (7, 7)
-save_folder = "../data/calliar/chars/"
-# plot 5x5 images from save_folder
-files = glob.glob(os.path.join(save_folder, "*.png"))[:25]
-fig, ax = plt.subplots(5, 5, sharex=True, sharey=True, subplot_kw={"xticks": [], "yticks": []})
-ax = ax.flatten()
-for i, file in enumerate(files):
-    ax = plt.subplot(5, 5, i + 1)
-    for axis in ["top", "bottom", "left", "right"]:
-        ax.spines[axis].set_visible(True)
-        ax.spines[axis].set_linewidth(1)
-        ax.spines[axis].set_color("black")
-    plt.imshow(plt.imread(file))
-    # plt.axis("off")
-    # plot label
-    label = file.split("/")[-1].split(":")[-1][:-4]
-    plt.title(label)
-plt.tight_layout()
+def plot_sample():
+    # plot a random sample of characters
+    plt.rcParams["figure.figsize"] = (7, 7)
+    save_folder = "../data/calliar/chars/"
+    # plot 5x5 images from save_folder
+    files = glob.glob(os.path.join(save_folder, "*.png"))[:25]
+    fig, ax = plt.subplots(5, 5, sharex=True, sharey=True, subplot_kw={"xticks": [], "yticks": []})
+    ax = ax.flatten()
+    for i, file in enumerate(files):
+        ax = plt.subplot(5, 5, i + 1)
+        for axis in ["top", "bottom", "left", "right"]:
+            ax.spines[axis].set_visible(True)
+            ax.spines[axis].set_linewidth(1)
+            ax.spines[axis].set_color("black")
+        plt.imshow(plt.imread(file))
+        # plt.axis("off")
+        # plot label
+        label = file.split("/")[-1].split(":")[-1][:-4]
+        plt.title(label)
+    plt.tight_layout()
 
 
-# %%
 # ## word-level strokes
 def draw_words(json_path, plot=False, save_folder="../data/calliar/words/", stroke_width=3, crop=True, square=True):
     if not os.path.exists(save_folder):
@@ -97,15 +73,6 @@ def draw_words(json_path, plot=False, save_folder="../data/calliar/words/", stro
             display(im)
 
 
-draw_words(npy_files[-8], plot=True)
-
-# %%
-from tqdm import tqdm
-
-for json_path in tqdm(npy_files):
-    draw_words(json_path, plot=False, stroke_width=np.random.randint(2, 8))
-
-# %%
 # ## sentence-level strokes
 def draw_sentences(
     json_path, plot=False, save_folder="../data/calliar/sentences/", stroke_width=3, crop=True, square=True
@@ -115,8 +82,8 @@ def draw_sentences(
     drawing = json.load(open(json_path))
     sentence = get_annotation(json_path)
     save_path = json_path.split("/")[-1][:-5] + ".png"
-    # if save_folder and os.path.exists(os.path.join(save_folder, save_path)):
-    #     return
+    if save_folder and os.path.exists(os.path.join(save_folder, save_path)):
+        return
     data, _ = convert_3d(drawing, return_flag=True, threshold=50)
     im = draw_strokes(data, stroke_width=stroke_width, crop=True, square=True)
     if save_folder:
@@ -126,10 +93,62 @@ def draw_sentences(
         display(im)
 
 
-draw_sentences(npy_files[-2], plot=True)
 
-# %%
-from tqdm import tqdm
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_path", type=str, default="../Calliar/")
+    parser.add_argument("--save_folder", type=str, default="../data/calliar/")
+    parser.add_argument("--level", type=str, default="sentences", help="sentences, words or chars")
+    parser.add_argument("--stroke_width", type=int, default=0)
+    parser.add_argument("--no_crop", action="store_true")
+    parser.add_argument("--no_square", action="store_true")
+    parser.add_argument("--plot", action="store_true")
+    args = parser.parse_args()
 
-for json_path in tqdm(npy_files):
-    draw_sentences(json_path, plot=False, stroke_width=np.random.randint(2, 8))
+    npy_files = glob.glob(os.path.join(args.dataset_path, "dataset/train/**.json"))
+    if len(npy_files) == 0:
+        raise ValueError("No json files found in {}".format(args.dataset_path))
+
+    # Preview a single file
+    # json_path = np.random.choice(npy_files)
+    # drawing = json.load(open(json_path))
+    # print(get_annotation(json_path))
+    # data, _ = convert_3d(drawing, return_flag=True, threshold=50)
+    # draw_strokes(data, stroke_width=8, crop=True, square=True)
+
+    print(f"{len(npy_files)} files found in {os.path.join(args.dataset_path, 'dataset/train/**.json')}")
+    print(f"Saving to {os.path.join(args.save_folder, args.level)}")
+
+    if args.level == "sentences":
+        for json_path in tqdm(npy_files):
+            draw_sentences(
+                json_path,
+                plot=args.plot,
+                save_folder=args.save_folder,
+                stroke_width=np.random.randint(2, 8) if args.stroke_width == 0 else args.stroke_width,
+                crop=not args.no_crop,
+                square=not args.no_square,
+            )
+    elif args.level == "words":
+        for json_path in tqdm(npy_files):
+            draw_words(
+                json_path,
+                plot=args.plot,
+                save_folder=args.save_folder,
+                stroke_width=np.random.randint(2, 8) if args.stroke_width == 0 else args.stroke_width,
+                crop=not args.no_crop,
+                square=not args.no_square,
+            )
+    elif args.level == "chars":
+        for json_path in tqdm(npy_files):
+            draw_chars(
+                json_path,
+                plot=args.plot,
+                save_folder=args.save_folder,
+                stroke_width=np.random.randint(2, 8) if args.stroke_width == 0 else args.stroke_width,
+                crop=not args.no_crop,
+                square=not args.no_square,
+            )
+    else:
+        raise ValueError("level must be one of sentences, words or chars")
+            
